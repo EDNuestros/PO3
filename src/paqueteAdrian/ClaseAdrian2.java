b7ffa189d81bec75f28c3e95c262f2b6e48a9456
Vespertino <Vespertino@C7PC17>
2015-04-24 20:59:11 +0200

    Commit directo Adrian

---
+++ src/paqueteAdrian/ClaseAdrian2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paqueteAdrian;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class ClaseAdrian2 {
+    public void Mensaje(){
+        System.out.println("Hola mundo Adrian 2");
+    }
+    
+}
